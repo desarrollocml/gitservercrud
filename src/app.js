aa5879ae9fa6da529f910cb30dd7141fc0f8d033
desarrollocml <cristianmirandalamas@gmail.com>
2021-04-10 19:27:17 -0400

    primr

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,8 @@ const indexRoutes = require('./routes/index');
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//app.set('views', 'src/views');//falta ruta de carpetas para reconocer los controladores
 
 app.use('/', indexRoutes);
 
